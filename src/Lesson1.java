345a0ce68a88a2a4264616c5936d2226d2a44c17
Dimaf <vladfilimontsev@yandex.ru>
2022-04-28 19:59:21 +0300

    tesr

---
+++ src/Lesson1.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+
+public class Lesson1 {
+    public static void main(String[] args) throws IOException {
+        /*int a = 5, b=15;
+        System.out.println("Hello World!");
+        System.out.printf("a = %d\nb = %d\n", a, b);
+        System.out.println("\" \" \" \\ \\ \\ \\ \\ \t\\");
+        System.out.print("Text first");
+        System.out.print('\b');
+        System.out.print("Yext second\n");
+        System.out.print("Text first");
+        System.out.print('\r');
+        System.out.print("New text\n");
+        System.out.print("Text first");
+        System.out.print('\b');
+        System.out.print("Yext second\n");
+        int i = System.in.read();
+        System.out.println(i);*/
+        /*Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(a);
+        BufferedReader bufin = new BufferedReader(new InputStreamReader(System.in));
+        int b = Integer.parseInt(bufin.readLine());
+        System.out.println(b);
+        Scanner myScan = new Scanner(System.in);
+        int a2 = myScan.nextInt();
+        System.out.println(a2);
+        int i1;
+        byte b1 = 127;
+        b1 = (byte) (b1 + 1);
+        System.out.println(b1);
+        long l;
+         */
+       /* float f = (float) 0.0001;
+        double d = 1. /8; //1d //()
+        System.out.println(d);
+        System.out.printf("%.20f", d);
+        for (int i = 0; i < 1000; i++) {
+            System.out.print((char)i + "");
+            if (i % 60 == 0) System.out.println(); */
+
+       int c = System.in.read();
+        System.out.println(c);
+    }
+    }
+
+
+
